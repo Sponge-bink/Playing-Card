2dc22a9cdb7dd47ee9c8d9bc441a81e86156b654
Sponge-bink <spongebink@gmail.com>
2019-02-03 15:17:40 +0800

    Copied from provided Main.storyboard code. more cards, but static.

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -13,42 +13,54 @@ class ViewController: UIViewController {
     private var deck = PlayingCardDeck()
     
     
-    @IBOutlet weak var playingCardView: PlayingCardView! {
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.left, .right]
-            playingCardView.addGestureRecognizer(swipe)
-            let pich = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustfFaceCardScale(byHandelingGestureReconizerBy:)))
-            playingCardView.addGestureRecognizer(pich)
-        }
-    }
+//    @IBOutlet weak var playingCardView: PlayingCardView! {
+//        didSet {
+//            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+//            swipe.direction = [.left, .right]
+//            playingCardView.addGestureRecognizer(swipe)
+//            let pich = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustfFaceCardScale(byHandelingGestureReconizerBy:)))
+//            playingCardView.addGestureRecognizer(pich)
+//        }
+//    }
+//
+//    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+//        switch sender.state {
+//        case .ended:
+//            playingCardView.isFaceup = !playingCardView.isFaceup
+//        default: break
+//        }
+//    }
+//
+//    @objc func nextCard() {
+//        if let card = deck.draw() {
+//            playingCardView.rank = card.rank.order
+//            playingCardView.suit = card.suit.rawValue
+//        }
+//    }
     
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended:
-            playingCardView.isFaceup = !playingCardView.isFaceup
-        default: break
-        }
-    }
     
-    @objc func nextCard() {
-        if let card = deck.draw() {
-            playingCardView.rank = card.rank.order
-            playingCardView.suit = card.suit.rawValue
-        }
-    }
+    @IBOutlet private var cardViews: [PlayingCardView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+
 //        for _ in 1...10 {
 //            if let card = deck.draw() {
 //                print("\(card)")
 //            }
 //        }
+        var cards = [PlayingCard]()
+        for _ in 1...((cardViews.count + 1) / 2) {
+            let card = deck.draw()!
+            cards += [card, card]
+        }
+        for cardView in cardViews {
+            cardView.isFaceup = true
+            let card = cards.remove(at: cards.count.arc4random)
+            cardView.rank = card.rank.order
+            cardView.suit = card.suit.rawValue
+        }
     }
-
-
 }
 
